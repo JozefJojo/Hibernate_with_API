0dbd9543a42e7b63bc6d1561745a73efd18491ec
Andrej <grilus.andok@gmail.com>
2022-08-28 11:57:43 +0200

    PP7

--- src/main/java/com/sda/cz5/WeatherMain.java
+++ src/main/java/com/sda/cz5/WeatherMain.java
@@ -7,6 +7,7 @@ import com.sda.cz5.weatherapi.LocationClient;
 import com.sda.cz5.weatherapi.LocationModel;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -36,17 +37,22 @@ public class WeatherMain {
         try {
             String[] commands = line.split(" ");
             switch (commands[0]) {
-                case "city-down" -> cityDown(commands[1]);
+                case "city-down" -> {
+                    LocationModel[] locationModels = cityDown(commands[1]);
+                    for (LocationModel model : locationModels) {
+                        System.out.println(model);
+                    }
+                }
                 case "store-city" -> storeCity(commands);
                 case "list-city" -> listCity();
                 case "switch-dao" -> switchDao(commands[1]);
-                case "help" ->help();
+                case "help" -> help();
                 default -> {
-                    System.out.println("Unknown command "+line);
+                    System.out.println("Unknown command " + line);
                 }
             }
-        }catch(Exception e){
-            resolveException(e,line);
+        } catch (Exception e) {
+            resolveException(e, line);
         }
     }
 
@@ -55,43 +61,62 @@ public class WeatherMain {
     }
 
     private void resolveException(Exception e, String line) {
-        System.out.println("Bad command format: "+line);
+        System.out.println("Bad command format: " + line);
         System.out.println("Try enter help");
     }
 
     private void switchDao(String command) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         if ("m".equals(command)) {
             locationDao = LocationFactory.createLocationDao(true);
-        }if("d".equals(command)){
+        }
+        if ("d".equals(command)) {
             locationDao = LocationFactory.createLocationDao(false);
-        }else{
+        } else {
             locationDao = LocationFactory.createLocationDao(command);
         }
     }
 
     private void listCity() {
         List<Location> all = locationDao.findAll();
-        for (Location location: all) {
+        for (Location location : all) {
             System.out.println(location);
         }
     }
 
-    private void storeCity(String[] commands) {
+    private void storeCity(String[] commands) throws IOException {
         String cityName = commands[1];
-        double lon = Double.parseDouble(commands[2]);
-        double lat = Double.parseDouble(commands[3]);
-
+        double lon = 0;
+        double lat = 0;
+        if (commands.length == 4) {
+            lon = Double.parseDouble(commands[2]);
+            lat = Double.parseDouble(commands[3]);
+        } else {
+            int index = commands.length==3? Integer.parseInt(commands[2]):1;
+            LocationModel[] locationModels = cityDown(cityName);
+            if(locationModels.length==1||commands.length==3){
+                lon = locationModels[index-1].getLon();
+                lat = locationModels[index-1].getLat();
+            }else {
+                System.out.println("Location can not be resolved");
+                System.out.println("Possible locations: ");
+                printArray(locationModels);
+                return;
+            }
+        }
         locationDao.saveLocation(Location.builder().
                 latitude(lat).
                 longitude(lon).
                 cityName(cityName).
                 build());
+
     }
 
-    private void cityDown(String cityName) throws IOException {
+    private void printArray(LocationModel[] locationModels) {
+         Arrays.stream(locationModels).forEach(System.out::println);
+    }
+
+    private LocationModel[] cityDown(String cityName) throws IOException {
         LocationModel[] location = client.getLocation(cityName);
-        for (LocationModel model : location) {
-            System.out.println(model);
-        }
+        return location;
     }
 }
