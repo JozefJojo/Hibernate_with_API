e139f4d332d676af1b5ece2a2e647af3d56fed87
martinve <martinvetsu@gmail.com>
2022-08-28 13:47:45 +0200

    PP 10 test location

--- src/main/java/com/sda/cz5/entity/BaseEntity.java
+++ src/main/java/com/sda/cz5/entity/BaseEntity.java
@@ -1,8 +1,10 @@
 package com.sda.cz5.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.ToString;
 
+@Data
 @MappedSuperclass
 public class BaseEntity {
     @ToString.Include
